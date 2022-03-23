1a82d2a272abae5658f9cb9447e6979916e91832
Keks <keks@htmlacademy.ru>
2022-03-23 18:37:40 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/.stylelintrc.js
@@ -0,0 +1,13 @@
+const { propertyOrdering, selectorOrdering } = require('stylelint-semantic-groups');
+
+
+module.exports = {
+  "extends": [
+    "stylelint-semantic-groups"
+  ],
+  plugins: ['stylelint-order'],
+  rules: {
+    'order/order': selectorOrdering, // to fine-tune configuration use selectorOrderFactory
+    'order/properties-order': propertyOrdering
+  },
+};
